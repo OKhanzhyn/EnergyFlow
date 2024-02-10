76cb75529795703a4480136043cc55b64263ccaa
Olena-Ignatenko <ignatenkoa2103@gmail.com>
2024-02-10 22:50:54 +0200

    update variables

--- src/js/quote.js
+++ src/js/quote.js
@@ -1,5 +1,8 @@
-// Задаємо ключі для локального сховища
-const LOCAL_KEY_DATE = 'quoteDate';
-const LOCAL_KEY_QUOTE = 'quote';
-const LOCAL_KEY_AUTHOR = 'quoteAuthor';
+import { getApiInfo } from './api.js';
+import { postApiInfo } from './api.js';
 
+const LOCAL_KEY_DATE = 'dateNow';
+const LOCAL_KEY_QUOTE = 'quoteDay';
+
+const quotePage = document.querySelector('.description');
+const authorPage = document.querySelector('.author-quote');
