289cfc1e4acfcffeade6b791244a08592cce764f
Merylly <142231027+Merylly@users.noreply.github.com>
2024-02-10 16:49:55 +0200

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -13,3 +13,5 @@ function getApiInfo({ filter, page = 1, limit = 12, type }) {
 function postApiInfo(userEmail, type) {
   return axios.post(`https://energyflow.b.goit.study/api/${type}`, userEmail);
 }
+
+export { getApiInfo, postApiInfo };
