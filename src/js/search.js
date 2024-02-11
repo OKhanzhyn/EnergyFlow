9eef2642788e7e72f9841691b199d60df8eff480
Julietro29 <mousemiu1@gmail.com>
2024-02-11 21:13:29 +0200

    Js for search-form

--- src/js/search.js
+++ src/js/search.js
@@ -5,6 +5,21 @@ document.addEventListener('DOMContentLoaded', function() {
   const form = document.getElementById('bp-form');
   const searchInput = document.querySelector('.bp-search-input');
   const bpList = document.querySelector('.bp-list');
+  const switchItems = document.querySelectorAll('.switch-item');
+  let page = 1; 
+  let limit = 8; 
+  let filter = '';
+  let subtype = '';
+
+    switchItems.forEach(item => {
+        item.addEventListener('click', function() {
+    switchItems.forEach(element => {
+        element.classList.remove('is-active');
+    });
+    this.classList.add('is-active');
+        filter = this.textContent.trim();
+    });
+});
 
   form.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -12,29 +27,35 @@ document.addEventListener('DOMContentLoaded', function() {
 
     bpList.innerHTML = '';
 
-    fetch(`/api/exercises?search=${searchTerm}`)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        if (data.length === 0) {
-          const noResultsMessage = document.createElement('div');
-          noResultsMessage.textContent = 'Unfortunately, no results were found. You may want to consider other search options to find the exercise you are looking for. Our range is wide and you have the opportunity to find more options that suit your needs.';
-          bpList.appendChild(noResultsMessage);
-        } else {
-          data.forEach(exercise => {
-            const listItem = document.createElement('li');
-            listItem.textContent = exercise.name;
-            bpList.appendChild(listItem);
-          });
-        }
-      })
-      .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-      });
+    axios.get(`/api/exercises`, {
+      params: {
+        search: searchTerm,
+        filter: filter,
+        subtype: subtype,
+        page: page,
+        limit: limit
+      }
+    })
+    .then(response => {
+      const data = response.data;
+      if (data.length === 0) {
+        iziToast.error({
+          title: 'No Results',
+          message: '',
+          position: 'topCenter',
+          timeout: 5000,
+          closeOnClick: true
+        });
+      } else {
+        data.forEach(exercise => {
+          const listItem = document.createElement('li');
+          listItem.textContent = exercise.name;
+          bpList.appendChild(listItem);
+        });
+      }
+    })
+        .catch(error => {
+            error;
+    });
   });
 });
-
