a2f1bdc97c1121683735b718d636b6004431c881
Merylly <142231027+Merylly@users.noreply.github.com>
2024-02-12 10:54:01 +0200

    add pagination

---
+++ src/js/pagination.js
@@ -0,0 +1,21 @@
+export const itemsPerPage = 8;
+export let currentPage = 1;
+
+export const mobileBreakpoint = 768;
+export const tabletBreakpoint = 1440;
+
+export function renderPagination(container, currentPage, totalPages) {
+  if (!container) {
+    console.error('Pagination container not found');
+    return;
+  }
+
+  let paginationHTML = '';
+  for (let i = 1; i <= totalPages; i++) {
+    paginationHTML += (
+      `<button class="page ${i === currentPage ? 'is-active' : ''}">${i}</button>`
+    );
+  }
+
+  return paginationHTML;
+}
