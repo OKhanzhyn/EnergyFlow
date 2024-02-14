367ccb01d0f48f54118542a730ba9de647bd4a6d
Viktoriia Kucher <142267781+torry455@users.noreply.github.com>
2024-02-14 23:58:45 +0200

    made export for capitalaizeFirst letter

--- src/js/exercises-list.js
+++ src/js/exercises-list.js
@@ -156,7 +156,7 @@ async function renderExerciseCards(exerciseData) {
 
 // Шаблон для картки вправ
 function renderExerciseCardMarkup(exercise) {
-    function capitalizeFirstLetter(word) {
+    export function capitalizeFirstLetter(word) {
         // Перевірка чи починається слово з букви
         if (/^[a-zA-Z]/.test(word)) {
             return word.charAt(0).toUpperCase() + word.slice(1);
