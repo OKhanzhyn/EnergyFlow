fd368194e7924574d7107a9a2dfb267501f596f2
Julietro29 <mousemiu1@gmail.com>
2024-02-15 20:17:26 +0200

    update search js

--- src/js/search.js
+++ src/js/search.js
@@ -7,98 +7,60 @@ document.addEventListener('DOMContentLoaded', async function () {
   const bpList = document.querySelector('.bp-list');
   const bpFormWrapper = document.querySelector('.bp-form-wraper');
   const bpSearchInput = document.querySelector('.bp-search-input');
-  const exercisesPerPage = 8;
-  let currentPage = 1; 
-
+  
+  // switch-item 
   for (let i = 0; i < switchItems.length; i++) {
     switchItems[i].addEventListener('click', async function() {
       const filter = switchItems[i].dataset.filter;
-      const subtypes = await fetchData(`subtypes?filter=${filter}`);
-      renderSubtypes(subtypes);
+
+      clearSearchResults();
+
+      // Виконуємо пошук у вправах що прийшли
+      const searchTerm = bpSearchInput.value.trim().toLowerCase();
+      performSearch('Muscles', 'All', searchTerm);
     });
   }
 
-  async function fetchData(url) {
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const contentType = response.headers.get('content-type');
-      if (contentType && contentType.includes('application/json')) {
-        return await response.json();
-      } else {
-        throw new Error('Response is not valid JSON');
-      }
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      return [];
-    }
-  }
-
-  function renderSubtypes(subtypes) {
-    exercisesList.innerHTML = '';
+  // Чистимо попередній пошук
+  function clearSearchResults() {
     bpList.innerHTML = '';
-    bpFormWrapper.classList.add('visually-hidden');
-    
-    if (!Array.isArray(subtypes)) {
-      console.error('Subtypes is not an array');
-      return;
-    }
+  }
 
-    for (let i = 0; i < subtypes.length; i++) {
-      const li = document.createElement('li');
-      li.textContent = subtypes[i];
-      li.addEventListener('click', async function() {
-        const exercises = await fetchData(`exercises?subtype=${subtypes[i]}`);
-        renderExercises(exercises);
-
-        bpFormWrapper.classList.remove('visually-hidden');
-      });
-      bpList.appendChild(li);
+  // Пошук
+  function performSearch(filter, subtype, searchTerm) {
+    const filteredExercises = Array.from(bpList.children).filter(exercise =>
+      exercise.textContent.toLowerCase().includes(searchTerm)
+    );
+    if (filteredExercises.length === 0) {
+      renderErrorMessage('Вправ не знайдено');
+    } else {
+      renderExercises(filteredExercises, bpList);
     }
   }
 
-  function renderExercises(exercises) {
-    exercisesList.innerHTML = '';
-    for (let i = 0; i < exercises.length; i++) {
-      const li = document.createElement('li');
-      li.textContent = exercises[i].name;
-      li.addEventListener('click', function() {
-        bpFormWrapper.classList.remove('visually-hidden');
-      });
-      exercisesList.appendChild(li);
-    }
-  }
-
+  // Помилка пошуку
   function renderErrorMessage(message) {
     exercisesList.innerHTML = `<li>${message}</li>`;
   }
 
-  async function performSearch(filter, subtype, searchTerm) {
-    const start = (currentPage - 1) * exercisesPerPage;
-    const url = `/api/exercises?filter=${filter}&subtype=${subtype}&search=${searchTerm}&start=${start}&limit=${exercisesPerPage}`;
-    try {
-      const response = await fetchData(url);
-      const exercises = response.exercises;
-      const total = response.total;
-      if (exercises && exercises.length === 0) {
-        renderErrorMessage('No exercises found.');
-      } else {
-        renderExercises(exercises);
-        updatePagination(Math.ceil(total / exercisesPerPage));
-      }
-    } catch (error) {
-      console.error('Error fetching exercises:', error);
-      renderErrorMessage('Error fetching exercises.');
+  function renderExercises(exercises, list) {
+    list.innerHTML = ''; // Очищаємо список
+    for (let i = 0; i < exercises.length; i++) {
+      const li = document.createElement('li');
+      li.textContent = exercises[i].textContent;
+      li.addEventListener('click', function() {
+        bpFormWrapper.classList.remove('visually-hidden');
+      });
+      list.appendChild(li);
     }
   }
-
+  
   bpSearchInput.addEventListener('input', function() {
     const searchTerm = bpSearchInput.value.trim().toLowerCase();
     performSearch('Muscles', 'All', searchTerm);
   });
 
+  // Філтр та підвид 
   document.getElementById('bp-form').addEventListener('submit', function(event) {
     event.preventDefault();
     const filter = document.querySelector('.switch-item.is-active').dataset.filter;
@@ -115,6 +77,7 @@ document.addEventListener('DOMContentLoaded', async function () {
     }
   });
 
+  //Перехід switchItems
   switchItems.forEach(item => {
     item.addEventListener('click', function() {
       if (!bpList.classList.contains('visually-hidden')) {
